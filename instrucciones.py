b9c0aff45d41b9ac83fcd8a6d57f8d6d22133217
dmaldo19 <93284983+dmaldo19@users.noreply.github.com>
2023-04-26 10:46:48 -0600

    Update instrucciones.py

--- instrucciones.py
+++ instrucciones.py
@@ -1,1 +1,15 @@
-instruccion = "";
+##Abrir el archivo de texto donde estan las instrucciones##
+with open('programa.txt', 'r') as archivo:
+    instrucciones = archivo.readlines();
+
+instrucciones = [linea.split('\n')[0] for linea in instrucciones];
+archivo.close();
+print(instrucciones);
+
+##Diccionario para las Instrucciones a utilizar:
+
+opcode = {"add":"000000", "sub":"000000", "or":"000000", "and":"000000", "slt":"000000", "nop":"000000"}
+
+shamt_function = {"add":"00000100000", "sub":"00000100010", "or":"00000100101", "and":"00000100100", "slt":"00000101010", "nop":"00000000000"}
+
+print(shamt_function["nop"]);
